72fae1d25f1a646c3695148565eef0b90d520b5f
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2020-06-26 20:34:20 +0200

    Creating a fake data repository to use on the UI previews

---
+++ app/src/main/java/br/com/alexandremarcondes/egginc/companion/FakeDataRepository.kt
@@ -0,0 +1,37 @@
+package br.com.alexandremarcondes.egginc.companion
+
+
+val user1 = ei.Ei.DeviceInfo.newBuilder()
+    .setDeviceId("55db18283aca1")
+    .setDeviceName("alex1")
+    .build()
+
+val user2 = ei.Ei.DeviceInfo.newBuilder()
+    .setDeviceId("55db18283aca2")
+    .setDeviceName("alex2")
+    .build()
+
+val user3 = ei.Ei.DeviceInfo.newBuilder()
+    .setDeviceId("55db18283aca3")
+    .setDeviceName("alex3")
+    .build()
+
+val user4 = ei.Ei.DeviceInfo.newBuilder()
+    .setDeviceId("55db18283aca4")
+    .setDeviceName("alex4")
+    .build()
+
+val user5 = ei.Ei.DeviceInfo.newBuilder()
+    .setDeviceId("55db18283aca5")
+    .setDeviceName("alex5")
+    .build()
+
+val fakeUsers: List<ei.Ei.DeviceInfo> =
+    listOf(
+        user1,
+        user2,
+        user3,
+        user4,
+        user5
+    )
+
